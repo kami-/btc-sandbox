7c089997af297951817adecf3a99c7bf43708f8f
László Major <laszlo.major@outlook.com>
2017-11-28 23:20:55 +0100

    Base58Check decoding

--- base58.js
+++ base58.js
@@ -1,6 +1,7 @@
 const bs58 = require("bs58");
 const crypto = require("./crypto");
 
+const BASE_58_CHECK_CHECKSUM_BYTE_SIZE = 4
 const BASE_58_CHECK_VERSION = {
     ADDRESS: Buffer.from([0x00]),
     PRIVATE_KEY_WIF: Buffer.from([0x80])
@@ -9,7 +10,7 @@ const BASE_58_CHECK_VERSION = {
 function checkEncode(version, payload) {
     const versionAndPayload = crypto.sha256(Buffer.concat([version, payload]));
     const checksum = crypto.sha256(versionAndPayload);
-    const result = Buffer.concat([version, payload, checksum.slice(0, 4)]);
+    const result = Buffer.concat([version, payload, checksum.slice(0, BASE_58_CHECK_CHECKSUM_BYTE_SIZE)]);
     return bs58.encode(result);
 }
 
@@ -17,7 +18,23 @@ function checkEncodeAddress(address) {
     return checkEncode(BASE_58_CHECK_VERSION.ADDRESS, address);
 }
 
+function checkDecodeAddress(encodedAddress) {
+    const decodedAddress = bs58.decode(encodedAddress);
+    const checksumStart = decodedAddress.length - BASE_58_CHECK_CHECKSUM_BYTE_SIZE - 1;
+    return {
+        version: decodedAddress[0],
+        address: Buffer.from(decodedAddress.slice(1, checksumStart)),
+        checksum: Buffer.from(decodedAddress.slice(checksumStart))
+    };
+}
+
+function checkEncodeWifPrivateKey(privateKey) {
+    return checkEncode(BASE_58_CHECK_VERSION.PRIVATE_KEY_WIF, privateKey);
+}
+
 module.exports = {
     checkEncode: checkEncode,
-    checkEncodeAddress: checkEncodeAddress
+    checkEncodeAddress: checkEncodeAddress,
+    checkDecodeAddress: checkDecodeAddress,
+    checkEncodeWifPrivateKey: checkEncodeWifPrivateKey
 }
--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const bigi = require("bigi");
 const address = require("./address");
 const base58 = require("./base58");
 
-const privateKey = bigi.fromHex("1E99423A4ED27608A15A2616A2B0E9E52CED330AC530EDCC32C8FFC6A526AEDD");
-const publicKey = address.generatePublicKey(privateKey.toBuffer());
+const privateKey = bigi.fromHex("1E99423A4ED27608A15A2616A2B0E9E52CED330AC530EDCC32C8FFC6A526AEDD").toBuffer();
+const publicKey = address.generatePublicKey(privateKey);
 const btcAddress = address.generateAddress(publicKey);
-console.log("Private hex:", privateKey.toHex());
+console.log("Private hex:", bigi.fromBuffer(privateKey).toHex());
+console.log("Private WIF:", base58.checkEncodeWifPrivateKey(privateKey));
 console.log("Public hex:", bigi.fromBuffer(publicKey).toHex());
 console.log("Address hex:", bigi.fromBuffer(btcAddress).toHex());
-console.log("Base58Check address:", base58.checkEncodeAddress(btcAddress));
+
+const checkEncodedBtcAddress = base58.checkEncodeAddress(btcAddress);
+console.log("Base58Check address:", checkEncodedBtcAddress);
+const decodedBtcAddress = base58.checkDecodeAddress(checkEncodedBtcAddress)
+console.log("Decoded Base58Check address:", decodedBtcAddress);
+console.log("Decoded Base58Check address hex:", bigi.fromBuffer(decodedBtcAddress.address).toHex());
